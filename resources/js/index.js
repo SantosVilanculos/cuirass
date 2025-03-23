d7e091e0dddc600efc062202ac373740353ea5ae
Santos Vilanculos <95357132+SantosVilanculos@users.noreply.github.com>
2025-03-23 03:44:19 +0200

    chore: add tabler

---
+++ resources/js/index.js
@@ -0,0 +1,13 @@
+import "@tabler/core/dist/js/tabler.min.js";
+
+// https://github.com/tabler/tabler/blob/dev/package.json
+
+// apexcharts
+// autosize
+// choices.js
+// countup.js
+// flatpickr
+// imask
+// litepicker
+// nouislider
+// tom-select
--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,16 @@
-import {
-    defineConfig
-} from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                "resources/css/app.css",
+                "resources/js/app.js",
+                "resources/css/index.css",
+                "resources/js/index.js",
+            ],
             refresh: [`resources/views/**/*`],
         }),
         tailwindcss(),
@@ -16,3 +19,4 @@ export default defineConfig({
         cors: true,
     },
 });
+
