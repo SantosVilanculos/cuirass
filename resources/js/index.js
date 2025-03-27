9ad46fcfa094477401a67100ccdaf0e0bf51d398
Santos Vilanculos <95357132+SantosVilanculos@users.noreply.github.com>
2025-03-27 11:49:20 +0200

    chore: add sweetalert2

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,13 +1,31 @@
-import "@tabler/core/dist/js/tabler.min.js";
+import '@tabler/core/dist/js/tabler.min.js';
+import Swal from 'sweetalert2';
 
-// https://github.com/tabler/tabler/blob/dev/package.json
+document.addEventListener('livewire:init', () => {
+    Livewire.on(
+        'message',
+        ({ titleText = undefined, text = undefined, icon = undefined, timer = 3_000, showCloseButton = false }) => {
+            // titleText?: string | undefined
+            // text?: string | undefined
+            // icon?: string<'success' | 'error' | 'warning' | 'info' | 'question'> | undefined
+            // timer?: number | undefined
+            // showCloseButton?: boolean | undefined
 
-// apexcharts
-// autosize
-// choices.js
-// countup.js
-// flatpickr
-// imask
-// litepicker
-// nouislider
-// tom-select
+            Swal.fire({
+                titleText,
+                text,
+                icon,
+                toast: true,
+                position: 'bottom',
+                timer,
+                timerProgressBar: true,
+                showConfirmButton: false,
+                showCloseButton,
+                didOpen: popup => {
+                    popup.onmouseenter = Swal.stopTimer;
+                    popup.onmouseleave = Swal.resumeTimer;
+                }
+            });
+        }
+    );
+});
