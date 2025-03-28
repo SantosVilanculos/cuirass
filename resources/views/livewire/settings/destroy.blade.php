07e74af9f323b8aac16fde3cd50eba5b44ebaeab
Santos Vilanculos <95357132+SantosVilanculos@users.noreply.github.com>
2025-03-28 22:07:55 +0200

    fix: delete account modal

--- resources/views/livewire/settings/destroy.blade.php
+++ resources/views/livewire/settings/destroy.blade.php
@@ -1,50 +1,52 @@
-<div class="">
-    <form action="./" method="POST">
+<div x-data="{ open: false }">
+    <div>
         <p class="text-body-secondary">
             Once your account is deleted, all of its resources and data will be permanently deleted. Before deleting
             your account, please download any data or information that you wish to retain.
         </p>
 
         <div class="form-footer">
-            <button class="btn btn-danger" type="button" data-bs-toggle="modal" data-bs-target="#modal">Delete</button>
+            <button x-on:click="open = true" class="btn btn-danger" type="button">Delete account</button>
         </div>
-    </form>
+    </div>
 
     @teleport('body')
-        <div class="modal modal-blur fade" id="modal" tabindex="-1" style="display: none" aria-hidden="true">
-            <div class="modal-dialog modal-md modal-dialog-centered" role="document">
-                <div class="modal-content">
-                    <div class="modal-body">
-                        <div class="modal-title">Are you sure?</div>
+        <div>
+            <div x-cloak x-show="open" class="modal-backdrop show"></div>
 
-                        <p class="text-body-secondary">
-                            If you proceed, you will lose all your personal data. Please enter your password to confirm
-                            that you would like to permanently delete your personal data.
-                        </p>
+            <div x-cloak x-show="open" x-transition:enter.scale.80 x-transition:leave.scale.90 class="modal modal-blur show"
+                tabindex="-1" aria-hidden="true">
+                <div x-on:click.outside="open = false" class="modal-dialog modal-md modal-dialog-centered" role="document">
+                    <form wire:submit="destroy1" class="modal-content">
+                        <div class="modal-body">
+                            @csrf
 
-                        <div class="mb-3">
-                            <label for="password" class="form-label">Current password</label>
-                            <input
-                                class="form-control"
-                                id="password"
-                                type="password"
-                                minlength="8"
-                                maxlength="20"
-                                autocomplete="current-password"
-                            />
-                            @error('password')
-                                <p class="invalid-feedback d-block">{{ $message }}</p>
-                            @enderror
+                            <div class="modal-title">Are you sure?</div>
+
+                            <p class="text-body-secondary">
+                                If you proceed, you will lose all your personal data. Please enter your password to confirm
+                                that you would like to permanently delete your personal data.
+                            </p>
+
+                            <div class="mb-3">
+                                <label for="password" class="form-label">Current password</label>
+                                <input wire:model="password" class="form-control" id="password" type="password"
+                                    minlength="8" maxlength="20" autocomplete="current-password" required />
+                                @error('password')
+                                    <p class="invalid-feedback d-block">{{ $message }}</p>
+                                @enderror
+                            </div>
                         </div>
-                    </div>
 
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-link link-secondary me-auto" data-bs-dismiss="modal">
-                            Cancel
-                        </button>
+                        <div class="modal-footer">
+                            <button x-on:click="open = false" type="button" class="btn btn-link link-secondary me-auto">
+                                Cancel
+                            </button>
 
-                        <button type="button" class="btn btn-danger">Yes, delete all my data</button>
-                    </div>
+                            <button wire:loading.class="btn-loading" type="submit" class="btn btn-danger">Yes, delete all
+                                my data</button>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
