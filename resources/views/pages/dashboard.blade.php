9b4e24edebed5ebc9273d7bcd768dd6d9f9c5d50
Santos Vilanculos <95357132+SantosVilanculos@users.noreply.github.com>
2025-03-29 00:01:04 +0200

    chore: add placeholder cards

--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -6,4 +6,66 @@
             </div>
         </div>
     @endsection
+
+    <div class="row row-cards">
+        <div class="col-md-4">
+            <div class="card">
+                <div class="card-body p-0">
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        class="w-100"
+                        preserveAspectRatio="none"
+                        width="400"
+                        height="150"
+                        viewBox="0 0 400 150"
+                        fill="transparent"
+                        stroke="var(--tblr-border-color, #b8cef1)"
+                    >
+                        <line x1="0" y1="0" x2="400" y2="150"></line>
+                        <line x1="0" y1="150" x2="400" y2="0"></line>
+                    </svg>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-4">
+            <div class="card">
+                <div class="card-body p-0">
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        class="w-100"
+                        preserveAspectRatio="none"
+                        width="400"
+                        height="150"
+                        viewBox="0 0 400 150"
+                        fill="transparent"
+                        stroke="var(--tblr-border-color, #b8cef1)"
+                    >
+                        <line x1="0" y1="0" x2="400" y2="150"></line>
+                        <line x1="0" y1="150" x2="400" y2="0"></line>
+                    </svg>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-4">
+            <div class="card">
+                <div class="card-body p-0">
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        class="w-100"
+                        preserveAspectRatio="none"
+                        width="400"
+                        height="150"
+                        viewBox="0 0 400 150"
+                        fill="transparent"
+                        stroke="var(--tblr-border-color, #b8cef1)"
+                    >
+                        <line x1="0" y1="0" x2="400" y2="150"></line>
+                        <line x1="0" y1="150" x2="400" y2="0"></line>
+                    </svg>
+                </div>
+            </div>
+        </div>
+    </div>
 </x-layouts.app>
