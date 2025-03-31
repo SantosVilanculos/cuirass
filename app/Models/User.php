1cb1de31876e03ecaf087451d146181275bb2fca
Santos Vilanculos <95357132+SantosVilanculos@users.noreply.github.com>
2025-03-31 02:31:39 +0200

    chore: add property annotations to User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,6 +9,17 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
+/**
+ * @property-read int $id
+ * @property string|null $image
+ * @property string $name
+ * @property string $email
+ * @property \Carbon\CarbonImmutable|null $email_verified_at
+ * @property string $password
+ * @property-read string|null $remember_token
+ * @property-read \Carbon\CarbonImmutable|null $created_at
+ * @property-read \Carbon\CarbonImmutable|null $updated_at
+ */
 class User extends Authenticatable implements MustVerifyEmail
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
