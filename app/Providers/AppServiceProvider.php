20f6f48d32cbf07a3d1e955aa8d27cb4b222c1ce
Santos Vilanculos <95357132+SantosVilanculos@users.noreply.github.com>
2025-04-04 19:20:55 +0200

    chore: prevent lazy loading when not in production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -32,6 +32,7 @@ class AppServiceProvider extends ServiceProvider
 
         DB::prohibitDestructiveCommands(App::isProduction());
 
+        Model::preventLazyLoading(! App::isProduction());
         Model::shouldBeStrict(! App::isProduction());
         Model::unguard();
 
