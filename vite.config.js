1abb6f70595223f1e742344e72d188a5d63418fa
Santos Vilanculos <santosvilanculos@yahoo.com>
2025-07-18 03:34:09 +0200

    feat: generate ide helper files automaticaly

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,24 @@
 import laravel from 'laravel-vite-plugin';
 import { defineConfig } from 'vite';
+import { run } from 'vite-plugin-run';
 
 export default defineConfig({
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: [`resources/views/**/*`]
+        }),
+        run({
+            input: [
+                {
+                    build: false,
+                    run: ['php', 'artisan', 'ide-helper:generate']
+                },
+                {
+                    run: ['php', 'artisan', 'ide-helper:models', '-N'],
+                    pattern: ['app/Models/**/*.php']
+                }
+            ]
         })
     ],
     server: {
