27fcc0eee2ddff7ba59d85d586a65548a7b75345
Santos Vilanculos <santosvilanculos@yahoo.com>
2025-07-18 03:24:40 +0200

    chore: update validation custom attributes

--- lang/en/validation.php
+++ lang/en/validation.php
@@ -195,6 +195,8 @@ return [
 
     'attributes' => [
         'email' => 'email address',
+        'name' => 'name',
+        'password' => 'password',
         'phone' => 'phone number',
         'user_id' => 'user',
     ],
